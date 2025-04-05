b1e1190fb48271b29aec03aa19cd9b441ccd868b
Alessandro <abcsandro@hotmail.com>
2025-04-05 01:17:29 -0300

    Remove unnecessary formatting in currency conversion

--- src/modules/gift/database/repositories/impl/typeorm/repositories/GiftRepositoryImpl.ts
+++ src/modules/gift/database/repositories/impl/typeorm/repositories/GiftRepositoryImpl.ts
@@ -23,7 +23,7 @@ export class GiftRepositoryImpl
     }
 
     async formatCurrency(value: string): Promise<number> {
-        return parseFloat(value.replace("R$ ", "").replace("R$ ", "").replace(".", "").replace(",", "."));
+        return parseFloat(value.replace("R$ ", "").replace("R$ ", ""));
     };
 
     async createItem(createGiftDTO: CreateGiftDTO, photo?: Express.Multer.File, guestId?: string): Promise<GiftDTO> {
